8664683ab06fc03f48d0b5fa702c9dce1f17d42d
Rick Belson <belsonrj@aol.com>
2020-01-24 04:59:52 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 speed = 50
 
 def unsafe?(speed)
-if speed > 60
-  return "unsafe"
-elsif speed < 40
-  return "unsafe"
-else
-  return "safe"
+  if speed > 60
+    return "unsafe"
+  elsif speed < 40
+    return "unsafe"
+  else
+    return "safe"
 
 end
 	
